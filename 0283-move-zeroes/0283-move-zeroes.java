47b55820a7e508e0488c9a17109381b78d433844
Akshat Prakhar <akshatprakhar299@gmail.com>
2026-07-20 18:58:00 +0530

    07-20-2026

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,26 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int j = -1;
+        int n = nums.length;
+
+        // Find first zero
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == 0) {
+                j = i;
+                break;
+            }
+        }
+
+        if (j == -1) return;
+
+        // Move non-zero elements forward
+        for (int i = j + 1; i < n; i++) {
+            if (nums[i] != 0) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                j++;
+            }
+        }
+    }
+}
