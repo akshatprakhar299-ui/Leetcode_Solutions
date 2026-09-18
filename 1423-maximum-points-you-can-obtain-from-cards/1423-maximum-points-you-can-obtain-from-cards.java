93e347a5ea87458ac265fe31aefb7b655bc97175
Akshat Prakhar <akshatprakhar299@gmail.com>
2026-09-19 01:04:10 +0530

    09-19-2026

---
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxScore(int[] cardPoints, int k) {
+        int n = cardPoints.length;
+        int lsum = 0;
+        int rsum = 0;
+        int maxsum = 0;
+        int rindex = n - 1;
+        for (int i = 0; i < k; i++) {
+            lsum += cardPoints[i];
+            maxsum = lsum;
+        }
+        for (int i = k - 1; i >= 0; i--) {
+            lsum -= cardPoints[i];
+            rsum += cardPoints[rindex];
+            rindex--;
+
+            maxsum = Math.max(maxsum, lsum + rsum);
+        }
+
+        return maxsum;
+    }
+}
